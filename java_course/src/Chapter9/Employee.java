7acdd826aa44bad0adba5171c50f3833309030bf
Spencer_Vail <vail.spencer@gmail.com>
2020-06-23 11:00:01 -0700

    Chapter 9a

---
+++ java_course/src/Chapter9/Employee.java
@@ -0,0 +1,28 @@
+package Chapter9;
+
+public class Employee extends Person
+{
+
+    private String EmployeeId;
+    private String title;
+
+    public String getEmployeeId()
+    {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(String employeeId)
+    {
+        EmployeeId = employeeId;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+}
---
+++ java_course/src/Chapter9/InheritanceTester.java
@@ -0,0 +1,11 @@
+package Chapter9;
+
+public class InheritanceTester
+{
+  public static void main(String[] args)
+  {
+      Person person = new Person();
+      Employee employee = new Employee();
+      employee.getEmployeeId();
+  }
+}
